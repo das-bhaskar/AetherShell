53412110ac9e8b2c4ca390ee0f3cee7723f07055
aloofjelly <aloofjelly@gmail.com>
2026-01-15 01:28:46 -0500

    created spring hub
    initialize Spring Boot hub and setup project-wide gitignore

---
+++ hub/src/main/java/bhaskar/aethershell/hub/AetherShellHubApplication.java
@@ -0,0 +1,13 @@
+package bhaskar.aethershell.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherShellHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherShellHubApplication.class, args);
+	}
+
+}
---
+++ hub/src/test/java/bhaskar/aethershell/hub/AetherShellHubApplicationTests.java
@@ -0,0 +1,13 @@
+package bhaskar.aethershell.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherShellHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
